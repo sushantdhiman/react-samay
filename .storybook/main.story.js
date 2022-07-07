852fe8188826f3e28e396879e963b9bba2c1e3f0
Sushant <sushantdhiman@outlook.com>
2022-07-07 18:20:30 +0530

    change: add more stories

--- .storybook/main.story.js
+++ .storybook/main.story.js
@@ -9,14 +9,19 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   padding-top: 10vh;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
     Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
 `;
 
-storiesOf('Timepicker', module).add('default', () => (
-  <Wrapper>
+storiesOf('Timepicker', module)
+  .addDecorator(Story => (
+    <Wrapper>
+      <Story />
+    </Wrapper>
+  ))
+  .add('default', () => (
     <TimePicker
       name="time"
       defaultValue={moment()}
@@ -25,5 +30,29 @@ storiesOf('Timepicker', module).add('default', () => (
         height: 50
       }}
     />
-  </Wrapper>
-));
+  ))
+  .add('use12Hours', () => (
+    <TimePicker
+      name="time"
+      use12Hours
+      defaultValue={moment()}
+      style={{
+        width: 400,
+        height: 50
+      }}
+    />
+  ))
+  .add('use12Hours - no seconds - step - readonly', () => (
+    <TimePicker
+      name="time"
+      inputReadOnly
+      use12Hours
+      minuteStep={15}
+      showSecond={false}
+      defaultValue={moment()}
+      style={{
+        width: 400,
+        height: 50
+      }}
+    />
+  ));
